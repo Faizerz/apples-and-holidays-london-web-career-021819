0a2e0765dc4343891e98644f804335f02c4b1ad9
Faiz Abbas <faiz_abbas@hotmail.co.uk>
2019-02-11 00:49:15 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -44,12 +44,9 @@ end
 
 def all_supplies_in_holidays(holiday_hash)
     holiday_hash.each do |season, festivals|
-      puts "#{season.capitalize}:"
+      puts "#{season.capitalize}: "
       festivals.each do |name, supplies|
-        puts "#{name.to_s.split('_').map do |word|
-          word.capitalize
-        end}.
-        }: "
+        puts "#{name.to_s.split('_').map {|word| word.capitalize }.join(' ') }: #{supplies.join(", ")}"
         end
       end
     end
